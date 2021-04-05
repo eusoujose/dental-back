696f94878640375d1579cd2b1d649f125ab151cd
eusoujose <ryuichi.nozema@gmail.com>
2021-04-04 23:30:00 -0300

    first commit

--- users/user.service.js
+++ users/user.service.js
@@ -1,30 +1,78 @@
 ﻿const config = require('config.json');
 const jwt = require('jsonwebtoken');
+const User = require('./models/user');
 
 // users hardcoded for simplicity, store in a db for production applications
-const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+const users = [
+    { id: 1, username: 'admin', password: 'shimaoka', name: 'José Poderoso', role: 'admin' },
+    { id: 2, username: 'operador', password: 'shimaoka', name: 'José Dentista' , role: 'operador'},
+    { id: 3, username: 'paciente', password: 'shimaoka', name: 'José Paciente' , role: 'user'},
+];
 
 module.exports = {
     authenticate,
-    getAll
+    getAllPatients,
+    createPatient,
+    getAllDentists,
+    createDentist
 };
 
-async function authenticate({ username, password }) {
-    const user = users.find(u => u.username === username && u.password === password);
-
-    if (!user) throw 'Username or password is incorrect';
+async function authenticate({ email, password }) {
+    const user = await User.findOne({email: email, password: password});
+    if (!user || user.length < 1) throw 'Username or password is incorrect';
 
     // create a jwt token that is valid for 7 days
-    const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
-
-    return {
-        ...omitPassword(user),
-        token
-    };
+    const token = jwt.sign({ sub: user.id, role: user.role }, config.secret, { expiresIn: '7d' });
+    const payload = {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+        createdAt: user.createdAt,
+        updateAt: user.updatedAt,
+        token: token
+    }
+    return payload;
 }
 
-async function getAll() {
-    return users.map(u => omitPassword(u));
+async function getAllPatients() {
+    return User.find({role: 'user'})
+        .then((result) => {
+            return result;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+async function getAllDentists() {
+    return User.find({role: 'operador'})
+        .then((result) => {
+            return result;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+async function createDentist(req) {
+    const checkIfUserExist = await User.findOne({email: req.email});
+    if (checkIfUserExist) throw 'Usuário já existe';
+    const user = new User(req);
+    user.role = 'operador';
+    user.save().then((result) => {
+        return result;
+    });
+}
+
+async function createPatient(req) {
+    const checkIfUserExist = await User.findOne({email: req.email});
+    if (checkIfUserExist) throw 'Usuário já existe';
+    const user = new User(req);
+    user.role = 'user';
+    user.save().then((result) => {
+        return result;
+    });
 }
 
 // helper functions
--- users/users.controller.js
+++ users/users.controller.js
@@ -4,7 +4,10 @@ const userService = require('./user.service');
 
 // routes
 router.post('/authenticate', authenticate);
-router.get('/', getAll);
+router.get('/pacientes', getAllPatients);
+router.post('/paciente', createPatient);
+router.get('/dentistas', getAllDentists);
+router.post('/dentista', createDentist);
 
 module.exports = router;
 
@@ -14,8 +17,26 @@ function authenticate(req, res, next) {
         .catch(next);
 }
 
-function getAll(req, res, next) {
-    userService.getAll()
+function getAllPatients(req, res, next) {
+    userService.getAllPatients()
         .then(users => res.json(users))
+        .catch(next);
+}
+
+function createPatient(req, res, next) {
+    userService.createPatient(req.body)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function getAllDentists(req, res, next) {
+    userService.getAllDentists()
+        .then(users => res.json(users))
+        .catch(next);
+}
+
+function createDentist(req, res, next) {
+    userService.createDentist(req.body)
+        .then(user => res.json(user))
         .catch(next);
 }
